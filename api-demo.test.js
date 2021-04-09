3dc13749d1cd08675bb722f34968644b222ad36a
Erick Dimistracopulos <edimistra@Ericks-MacBook-Pro-Theorem.local>
2021-04-09 16:16:55 -0400

    Jest API Snapshot initial commit

---
+++ api-demo.test.js
@@ -0,0 +1,31 @@
+const { expect, test } = require('@jest/globals')
+const axios = require('axios')
+
+function sanitize(data, keys) {
+    return keys.reduce((result, key) => {
+      const val = _.get(result, key);
+      if(!val || _.isArray(val) || _.isObject(val)) {
+        return result;
+      } else {
+        return _.set(_.cloneDeep(result), key, '[SANITIZED]');
+      }
+    }, data);
+  }
+
+test('Get all Ads', (done) => {
+    axios.get('http://localhost:3001')
+        .then((response) => {
+            expect(response).toMatchSnapshot()
+            done()
+        }, (error) => {
+            done(error)
+        })
+})
+
+test('Insert a new Ad', async () => {
+    const response = await axios.post('http://localhost:3001', {
+        title: 'Pizza',
+        price: 10.5
+    })
+    expect(response).toMatchSnapshot()
+})
---
+++ src/database/ads.js
@@ -0,0 +1,43 @@
+// ./src/database/ads.js
+const {getDatabase} = require('./mongo');
+const {ObjectID} = require('mongodb');
+
+const collectionName = 'ads';
+
+async function insertAd(ad) {
+  const database = await getDatabase();
+  const {insertedId} = await database.collection(collectionName).insertOne(ad);
+  return insertedId;
+}
+
+async function getAds() {
+  const database = await getDatabase();
+  return await database.collection(collectionName).find({}).toArray();
+}
+
+async function deleteAd(id) {
+    const database = await getDatabase();
+    await database.collection(collectionName).deleteOne({
+      _id: new ObjectID(id),
+    });
+  }
+  
+  async function updateAd(id, ad) {
+    const database = await getDatabase();
+    delete ad._id;
+    await database.collection(collectionName).update(
+      { _id: new ObjectID(id), },
+      {
+        $set: {
+          ...ad,
+        },
+      },
+    );
+  }
+
+module.exports = {
+  insertAd,
+  getAds,
+  deleteAd,
+  updateAd,
+};
