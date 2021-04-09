61f3f4199882d4236a804ff2cdf0fd52acd2dae3
Erick Dimistracopulos <edimistra@Ericks-MacBook-Pro-Theorem.local>
2021-04-09 16:54:16 -0400

    Updated

--- api-demo.test.js
+++ api-demo.test.js
@@ -1,5 +1,6 @@
 const { expect, test } = require('@jest/globals')
 const axios = require('axios')
+const {server, close} = require('./src/server')
 
 function sanitize(data, keys) {
     return keys.reduce((result, key) => {
@@ -12,20 +13,31 @@ function sanitize(data, keys) {
     }, data);
   }
 
-test('Get all Ads', (done) => {
-    axios.get('http://localhost:3001')
-        .then((response) => {
-            expect(response).toMatchSnapshot()
-            done()
-        }, (error) => {
-            done(error)
-        })
-})
-
-test('Insert a new Ad', async () => {
-    const response = await axios.post('http://localhost:3001', {
-        title: 'Pizza',
-        price: 10.5
+  describe('Run all API tests', () => {
+    beforeAll(async () => {
+      await server()
     })
-    expect(response).toMatchSnapshot()
-})
+
+    afterAll(() => {
+      close()
+    })
+
+    test('Get all Ads', (done) => {
+      axios.get('http://localhost:3001')
+          .then((response) => {
+              expect(response).toMatchSnapshot()
+              done()
+          }, (error) => {
+              done(error)
+          })
+  })
+  
+  test('Insert a new Ad', async () => {
+      const response = await axios.post('http://localhost:3001', {
+          title: 'Pizza',
+          price: 10.5
+      })
+      expect(response).toMatchSnapshot()
+  })
+  })
+
---
+++ src/server.js
@@ -0,0 +1,80 @@
+// importing the dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const {startDatabase} = require('./database/mongo');
+const {insertAd, getAds, deleteAd, updateAd} = require('./database/ads');
+
+// defining the Express app
+const app = express();
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+// defining an endpoint to return all ads
+app.get('/', async (req, res) => {
+    res.send(await getAds());
+  });
+
+app.post('/', async (req, res) => {
+  const newAd = req.body;
+  await insertAd(newAd);
+  res.send({ message: 'New ad inserted.' });
+});
+
+// endpoint to delete an ad
+app.delete('/:id', async (req, res) => {
+  await deleteAd(req.params.id);
+  res.send({ message: 'Ad removed.' });
+});
+
+// endpoint to update an ad
+app.put('/:id', async (req, res) => {
+  const updatedAd = req.body;
+  await updateAd(req.params.id, updatedAd);
+  res.send({ message: 'Ad updated.' });
+});
+
+app.post('/', async (req, res) => {
+    const newAd = req.body;
+    await insertAd(newAd);
+    res.send({ message: 'New ad inserted.' });
+});
+  
+// endpoint to delete an ad
+app.delete('/:id', async (req, res) => {
+    await deleteAd(req.params.id);
+    res.send({ message: 'Ad removed.' });
+});
+  
+// endpoint to update an ad
+app.put('/:id', async (req, res) => {
+    const updatedAd = req.body;
+    await updateAd(req.params.id, updatedAd);
+    res.send({ message: 'Ad updated.' });
+});
+
+module.exports = {
+    server: async function() {
+        await startDatabase();
+    
+        // starting the server
+        app.listen(3001, () => {
+            console.log('listening on port 3001');
+        });
+    },
+    close: function() {
+        app.close()
+    }
+}
